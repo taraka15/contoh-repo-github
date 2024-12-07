31f2ef44a7377b02518dc3192034d4dbdaccce0e
taraka15 <rexbhan15@gmail.com>
2024-12-07 19:16:50 +0700

    Tugas Akhir

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript: Handle "Add to Cart" Button
+document.querySelectorAll('.add-to-cart').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Product added to cart!');
+    });
+});
